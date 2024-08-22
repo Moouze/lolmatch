31a0b1c2bce6dc3be9fe1ac52bbbafb6103db4ad
Lucas <lucas_dev@outlook.com.br>
2024-08-22 18:56:37 -0300

    First Commit

---
+++ src/main/java/com/moouze/lolmatch/LolmatchApplication.java
@@ -0,0 +1,13 @@
+package com.moouze.lolmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolmatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moouze/lolmatch/LolmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moouze.lolmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
